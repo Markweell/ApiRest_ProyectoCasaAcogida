bf371fecf21e408be7afceb62265f94a4afd747f
José Rafael Alvarez Espino <jose.alvarez@atmira.com>
2019-05-09 13:59:29 +0200

    Updated

--- src/routes.php
+++ src/routes.php
@@ -15,6 +15,7 @@ require 'routesFunction/obtenerPaises.php';
 require 'routesFunction/obtenerSexos.php';
 require 'routesFunction/obtenerCamasLibres.php';
 require 'routesFunction/obtenerPersonasFueraDeCasa.php';
+require 'routesFunction/obtenerOcupacion.php';
 //POST
 require 'routesFunction/forgotPassword.php';
 require 'routesFunction/validateToken.php';
@@ -44,6 +45,7 @@ $app->group('/api', function () use ($app) {
         $app->get('/obtenerSexos', 'obtenerSexos');
         $app->get('/obtenerCamasLibres', 'obtenerCamasLibres');
         $app->get('/obtenerPersonasFueraDeCasa', 'obtenerPersonasFueraDeCasa');
+        $app->get('/obtenerOcupacion', 'obtenerOcupacion');
         $app->post('/forgotPassword', 'forgotPassword');
         $app->post('/validateToken', 'validateToken');
         $app->post('/changePassword', 'changePassword');
