f0947cb5deca24e6c6469a190eaea276c9d05ef6
Marcos <marcosgallardoperez@gmail.com>
2019-05-13 23:25:23 +0200

    Obtener Ficha personal en una consulta

--- src/routesFunction/obtenerFichaPersonal.php
+++ src/routesFunction/obtenerFichaPersonal.php
@@ -6,22 +6,55 @@ function obtenerFichaPersonal($response, $request, $next){
     $conexion = \Conexion::getConnection();
     $resp = json_decode($response->getBody());
     $id = $resp->id;
+    $valor=[':id'=>$id];
+    // $consulta = $conexion->prepare(
+    //     'SELECT 
+    //         fichas_personas.*,
+    //         t_paises1.nacionalidad,
+    //         t_sexo.sexo,
+    //         t_paises2.nacionalidad as "lugarNacimiento"
+    //     FROM fichas_personas 
+    //         LEFT JOIN t_paises AS t_paises1 
+    //             ON fichas_personas.idNacionalidad = t_paises1.id 
+    //         LEFT JOIN t_sexo 
+    //             ON fichas_personas.idSexo = t_sexo.id 
+    //         LEFT JOIN t_paises AS t_paises2 
+    //             ON fichas_personas.idPaisNacimiento = t_paises2.id 
+    //     WHERE fichas_personas.id = :id');
+        
+    // $consulta->execute($valor);
+    // $resultadoMainData=$consulta->fetchAll();
+    
     $consulta = $conexion->prepare(
         'SELECT 
             fichas_personas.*,
             t_paises1.nacionalidad,
             t_sexo.sexo,
-            t_paises2.nacionalidad as "lugarNacimiento"
+            t_paises2.nacionalidad as "lugarNacimiento",
+            registro.fecha_ingreso,
+            registro.fecha_salida,
+            r_registro_camas.idCama,
+            r_registro_camas.fecha_inicio AS "asignacionCama",
+            r_registro_camas.fecha_final AS "abandonoCama",
+            camas.cama AS "numeroCama",
+            habitaciones.habitacion AS "habitacion"
         FROM fichas_personas 
             LEFT JOIN t_paises AS t_paises1 
                 ON fichas_personas.idNacionalidad = t_paises1.id 
             LEFT JOIN t_sexo 
                 ON fichas_personas.idSexo = t_sexo.id 
             LEFT JOIN t_paises AS t_paises2 
-                ON fichas_personas.idPaisNacimiento = t_paises2.id 
+                ON fichas_personas.idPaisNacimiento = t_paises2.id
+            LEFT JOIN registro
+   	            ON fichas_personas.id = registro.idFichaPersona
+            LEFT JOIN r_registro_camas
+   	            ON registro.id = r_registro_camas.idRegistro
+            LEFT JOIN camas
+   	            ON r_registro_camas.idCama = camas.id
+            LEFT JOIN habitaciones
+   	            ON camas.idHabitacion = habitaciones.habitacion
         WHERE fichas_personas.id = :id');
-        
-    $consulta->execute([':id'=>$id]);
+    $consulta->execute($valor);
     $resultadoMainData=$consulta->fetchAll();
 
     if($resultadoMainData)
