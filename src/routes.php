ecc2612b8b26db5fc4ae8ad7aea737d8824a58f1
José Rafael Alvarez Espino <jose.alvarez@atmira.com>
2019-06-03 13:52:59 +0200

    updated

--- src/routes.php
+++ src/routes.php
@@ -65,6 +65,9 @@ require 'routesFunction/agregarNuevaSalida.php';
 require 'routesFunction/cambiarCama.php';
 require 'routesFunction/obtenerProvincias.php';
 require 'routesFunction/obtenerMunicipios.php';
+require 'routesFunction/obtenerInformeIngresosPorSexoAnnio.php';
+require 'routesFunction/obtenerInformeIngresosPorSexoAnnioMes.php';
+
 
 use Slim\Http\Request;
 use Slim\Http\Response;
@@ -132,6 +135,8 @@ $app->group('/api', function () use ($app) {
         $app->post('/cambiarCama','cambiarCama');
         $app->post('/obtenerProvincias','obtenerProvincias');
         $app->post('/obtenerMunicipios','obtenerMunicipios');
+        $app->post('/obtenerInformeIngresosPorSexoAnnio', 'obtenerInformeIngresosPorSexoAnnio');
+        $app->post('/obtenerInformeIngresosPorSexoAnnioMes', 'obtenerInformeIngresosPorSexoAnnioMes');
     });
   });
 
